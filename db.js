d830a76c389f16512907f36e3c7ea87631ac2fbb
IbnuAhsani <ibnu.muhari@gmail.com>
2019-11-05 09:26:16 +0700

    feat: add static data on HTML page refresh

--- db.js
+++ db.js
@@ -9,8 +9,22 @@ dbReq.onupgradeneeded = event => {
 
 dbReq.onsuccess = event => {
   db = event.target.result;
+
+  addStickyNote(db, "Sloths are awesome!");
+  addStickyNote(db, "I need to go to the bathroom!");
 };
 
 dbReq.onerror = event => {
   alert("error opening database " + event.target.errorCode);
+};
+
+const addStickyNote = (db, message) => {
+  let tx = db.transaction(["notes"], "readwrite");
+  let store = tx.objectStore("notes");
+  let note = { text: message, timestamp: Date.now() };
+
+  store.add(note);
+
+  tx.oncomplete = () => console.log("stored note!");
+  tx.onerror = event => alert("error storing note " + event.target.errorCode);
 };
